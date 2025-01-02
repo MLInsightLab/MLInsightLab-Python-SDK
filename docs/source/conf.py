bb3590e1521d5aa72b91e35e843eb490c3eb92cb
Ian Sotnek <ian.sotnek@gmail.com>
2025-01-02 17:19:52 -0500

    Adding Sphinx docs and GH actions workflow
    Signed-off-by: Ian Sotnek <ian.sotnek@gmail.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ML Insight Lab Python SDK'
+copyright = '2025, Jacob Renn, Ian Sotnek'
+author = 'Jacob Renn, Ian Sotnek'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+extensions = ['sphinx_rtd_theme',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.intersphinx'
+              ]
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src'))
---
+++ docs/source/index.rst
@@ -0,0 +1,18 @@
+.. ML Insight Lab Python SDK documentation master file, created by
+   sphinx-quickstart on Thu Jan  2 15:40:45 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ML Insight Lab Python SDK documentation
+=======================================
+Welcome to the ML Insight Lab Python SDK documentation. This SDK provides a programmatic interface to interact with the ML Insight Lab platform.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   MLILClient
+==================
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
