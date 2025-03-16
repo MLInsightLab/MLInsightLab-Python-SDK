f4123e6b32dcba321e44b14e14638eecfc786e5e
Jacob Renn <jacob.renn4@gmail.com>
2025-03-16 19:04:46 -0400

    updating version
    Signed-off-by: Jacob Renn <jacob.renn4@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mlinsightlab',
-    version='0.0.18',
+    version='0.0.19',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=open('requirements.txt').read().splitlines(),
