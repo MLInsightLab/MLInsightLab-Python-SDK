980032bf102fb65d160a9fe08795d7d6e2f81e3c
Jacob Renn <jacob.renn4@gmail.com>
2025-04-17 11:05:36 -0400

    updating favicon
    Signed-off-by: Jacob Renn <jacob.renn4@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ extensions = ['sphinx_rtd_theme',
               ]
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = '_static/logo.png'
+html_favicon = '_static/logo.png'
 html_static_path = ['_static']
 
 
